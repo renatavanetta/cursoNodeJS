a8f853af6053c744efe1b57a41f4046ef1fba4a2
Renata Vanetta <renatavanetta@outlook.com>
2021-12-29 19:18:32 -0300

    fundamentos do npm

--- 2. Core_Modules/15_modulo_OS/index.js
+++ 2. Core_Modules/15_modulo_OS/index.js
@@ -2,21 +2,16 @@ const os = require('os')
 const chalk = require('chalk')
 
 console.log(chalk.bgGray.black('CPUS'))
-console.log(os.cpus())
-console.log('\n')
+console.log(os.cpus(), "\n")
 
 console.log(chalk.bgGray.black('MEMORIA LIVRE'))
-console.log(os.freemem() + ' mb')
-console.log('\n')
+console.log(os.freemem() + ' mb', "\n")
 
 console.log(chalk.bgGray.black('DIRETORIO PRINCIPAL'))
-console.log(os.homedir())
-console.log('\n')
+console.log(os.homedir(), "\n")
 
 console.log(chalk.bgGray.black('RELEASE'))
-console.log(os.release())
-console.log('\n')
+console.log(os.release(), "\n")
 
 console.log(chalk.bgGray.black('TIPO'))
-console.log(os.type())
-console.log('\n')
+console.log(os.type(), "\n")
---
+++ 3. NPM_Fundamental/1_Instalando_modulos/index.js
@@ -0,0 +1,7 @@
+const _ = require('lodash')
+
+const a = [1, 2, 4, 6, 10]
+const b = [2, 5, 6, 10, 12]
+
+console.log(_.difference(a,b))
+
