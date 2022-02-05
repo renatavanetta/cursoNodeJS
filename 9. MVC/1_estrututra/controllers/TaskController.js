a4602f39163998a63af4b63d678c6c52bc162fd2
Renata Vanetta <renatavanetta@outlook.com>
2022-02-05 15:27:34 -0300

    mvc inicio

---
+++ 9. MVC/1_estrututra/controllers/TaskController.js
@@ -0,0 +1,11 @@
+const task = require('../models/Task')
+
+module.exports = class TaskController {
+    static createTask(req, res) {
+        res.render('tasks/create')
+    }
+
+    static showTasks(req, res) {
+        res.render('tasks/all')
+    }
+}
